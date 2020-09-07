a4cce97ff2aad1dbf260313867822968b30650e1
agrajak <korbots@gmail.com>
2020-09-07 22:10:26 +0900

    add emotion for css in js

--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,32 @@
 import React from "react"
+import { Link } from "gatsby"
+import styled from "@emotion/styled"
+
+const Ol = styled.ol`
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  justify-content: space-evenly;
+`
+const Li = styled.li`
+  text-decoration: none;
+`
+
 export default function Header() {
-  return <div>hi i'm header</div>
+  return (
+    <header>
+      <nav>
+        <Ol>
+          <Li class="logo">agrajak.log</Li>
+          <Li>
+            <Link to="/">홈</Link>
+          </Li>
+          <Li>
+            <Link to="/about">소개</Link>
+          </Li>
+          <Li>글 목록</Li>
+        </Ol>
+      </nav>
+    </header>
+  )
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,18 @@
 import React from "react"
 import Header from "./header"
 import Footer from "./footer"
+import styled from "@emotion/styled"
+
+const Component = styled.div`
+  padding: 10px 100px;
+`
 
 export default function Layout({ children }) {
   return (
-    <>
+    <Component>
       <Header></Header>
       {children}
       <Footer></Footer>
-    </>
+    </Component>
   )
 }
